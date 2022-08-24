2f0188d4094f520e3747b68cb9deee38eed9b426
William Cleisson de Carvalho <williamcc89@gmail.com>
2022-08-24 10:45:55 -0300

    Initial publication

---
+++ README.md
@@ -0,0 +1,72 @@
+bunyan-mysql2
+====================
+
+A Bunyan stream for saving logs into Mysql.
+
+## Install
+
+```
+npm install bunyan-mysql2
+```
+
+## Create Table
+
+Create table in your host
+
+```
+CREATE TABLE log(
+	name varchar(200),
+	hostname varchar(200),
+	msg varchar(21845),
+	pid integer,
+	level integer,
+	time datetime,
+	v integer
+);
+```
+
+## Example
+
+```
+var bunyan      = require('bunyan');
+var MySqlStream = require('bunyan-mysql2');
+
+var msqlStream = new MySqlStream({
+    host     : 'localhost',
+    user     : 'user',
+    password : 'pass',
+    database : 'database',
+    port     : 3306
+});
+
+msqlStream.on('error', function (err) {
+    console.log('MySQL Stream Error:', err.stack);
+});
+
+var log = bunyan.createLogger({
+    name: "My Application",
+    streams: [
+        { stream: process.stdout },
+        { stream: msqlStream }
+    ],
+    serializers: bunyan.stdSerializers
+});
+
+log.info('Oh My God, this works!!!');
+
+```
+
+## Options
+
+* `host`: The hostname of the database you are connecting to. (Default: `localhost`)
+* `user`: The MySQL user to authenticate as.
+* `password`: The password of that MySQL user.
+* `database`: Name of the database to use for this connection (Optional).
+* `port`: The port number to connect to. (Default: `3306`)
+* `table`: Name of table log. (Default: `log`)
+* `connectionLimit`: The maximum number of connections to create at once.
+  (Default: `10`)
+
+  ## Credits
+
+Package developed based on `bunyan-mysql` package.
---
+++ index.js
@@ -0,0 +1,63 @@
+"use strict";
+
+/***
+ * @author William Cleisson de Carvalho
+ * @date 24/08/2022.
+ */
+var Writable = require('stream').Writable;
+var util     = require('util');
+var mysql    = require('mysql2');
+var moment   = require('moment');
+
+var levels = {
+  10: 'trace',
+  20: 'debug',
+  30: 'info',
+  40: 'warn',
+  50: 'error',
+  60: 'fatal'
+};
+
+function MySqlStream(options){
+  this._options = options || {};
+  this._options.table = options.table || 'log';
+  this._conn  =  mysql.createPool({
+    connectionLimit : (options.connectionLimit || 10),
+    host            : options.host,
+    user            : options.user,
+    password        : options.password,
+    database        : options.database
+  });
+  Writable.call(this, {});
+}
+
+util.inherits(MySqlStream, Writable);
+
+MySqlStream.prototype._write = function(entry, encoding , callback){
+
+    entry = JSON.parse(entry.toString('utf8'));
+    var sql = "INSERT INTO <DATABASE>.<TABLE> "+
+            "( name, hostname, msg, pid, level, time, v) "+
+            "VALUES ("+
+            mysql.escape(entry.name) + ", " +
+            mysql.escape(entry.hostname) + ", " +
+            mysql.escape(entry.msg) + ", " +
+            mysql.escape(entry.pid) + ", " +
+            mysql.escape(entry.level) + ", " +
+            "STR_TO_DATE(" + mysql.escape(moment(entry.time).format("YYYY-MM-DD HH:mm:ss")) + ",'%Y-%m-%d %H:%i:%s'), " +
+            mysql.escape(entry.v) + ") ";
+
+    sql = sql.replace("<DATABASE>", this._options.database);
+    sql = sql.replace("<TABLE>", this._options.table);
+
+    var self = this;
+
+    this._conn.query(sql, entry, function(err) {
+        if (err) self.emit('error', err);
+            if (err) self.emit('error', err);
+    });
+    callback();
+};
+
+module.exports = MySqlStream;
+
